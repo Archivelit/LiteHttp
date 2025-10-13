6943f7fe0872a97f6383c84489286a7e3f63d087
Archivelit <dalbaeb1488322666@gmail.com>
2025-10-13 18:08:12 +0200

    Fixed incorrect headers parsing in RequestParser

--- src/RequestProcessors/RequestParser.cs
+++ src/RequestProcessors/RequestParser.cs
@@ -2,13 +2,16 @@
 
 public sealed class RequestParser : IRequestParser
 {
-    public HttpContext Parse(string request)
+    public HttpContext? Parse(string request)
     {
         var splitedRequest = GetSplitedRequest(request);
+        if (splitedRequest.Length < 2)
+            throw new ArgumentException("The request has unsupported or incorrect format");
+
         var headers = splitedRequest[0];
         var body = splitedRequest[1];
 
-        return new HttpContext(ParseHeadersToDictionary(headers), body);
+        return new(ParseHeadersToDictionary(headers), body);
     }
 
     private string[] GetSplitedRequest(string request) =>
@@ -19,7 +22,7 @@ public sealed class RequestParser : IRequestParser
     {
         var splitedHeaders = headers.Split("\r\n");
         var headersDictionary = new Dictionary<string, string>(splitedHeaders.Length, StringComparer.OrdinalIgnoreCase);
-        
+
         for (int i = 0; i < splitedHeaders.Length; i++)
         {
             var index = splitedHeaders[i].IndexOf(':');
@@ -27,9 +30,9 @@ public sealed class RequestParser : IRequestParser
             if (index == -1)
                 throw new FormatException("Headers must contain ':' symbol");
 
-            var key = headers[..index].Trim();
-            var value= headers[(index+1)..].Trim();
-            
+            var key = splitedHeaders[i][..index].Trim();
+            var value = splitedHeaders[i][(index + 1)..].Trim();
+
             headersDictionary.Add(key, value);
         }
 
