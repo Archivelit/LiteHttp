0aeb3f4a1eff5eaafd416fa25927d38857830b24
Archivelit <dalbaeb1488322666@gmail.com>
2025-11-18 15:03:44 +0100

    Refactor parser and add documentation

--- src/LiteHttp.Server/Internal/RequestProcessors/Parser.cs
+++ src/LiteHttp.Server/Internal/RequestProcessors/Parser.cs
@@ -5,14 +5,16 @@ internal sealed class Parser : IParser
 {
     public static readonly Parser Instance = new();
 
+    /// <inheritdoc/>
+    /// <returns><see cref="Result{TResult}"/> wrapee with result or exception wrapped</returns>
+    /// <exception cref="ArgumentException">Returned in <see cref="Result{TResult}"/> wrapee if request has no route or method
+    /// </exception>
+    [SkipLocalsInit]
     public Result<HttpContext> Parse(Memory<byte> request)
     {
         var requestParts = SplitRequest(request);
 
-        if (!requestParts.Success)
-            return new(requestParts.Exception);
-
-        var firstLine = GetFirstLine(requestParts.Value.Headers);
+        var firstLine = GetFirstLine(requestParts.Headers);
 
         var method = GetMethod(firstLine);
         
@@ -24,11 +26,20 @@ internal sealed class Parser : IParser
         if (!route.Success)
             return new (route.Exception);
 
-        var headerSection = requestParts.Value.Headers[(firstLine.Length + RequestSymbolsAsBytes.NewRequestLine.Length)..]; // First line of request does not contain any header
+        var headerSection = requestParts.Headers[(firstLine.Length + RequestSymbolsAsBytes.NewRequestLine.Length)..]; // First line of request does not contain any header
 
-        return new Result<HttpContext>(new HttpContext(method.Value, route.Value, MapHeaders(headerSection).Value, requestParts.Value.Body));
+        var headers = MapHeaders(headerSection);
+        
+        return !headers.Success 
+            ? new(headers.Exception) 
+            : new Result<HttpContext>(new HttpContext(method.Value, route.Value, headers.Value, requestParts.Body));
     }
 
+    /// <summary>
+    /// Extracts route from <paramref name="firstRequestLine"/>.
+    /// </summary>
+    /// <param name="firstRequestLine">The first line of http request represented in byte array</param>
+    /// <returns><see cref="Result{TReult}"/> wrapee with result or exception wrapped</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private Result<Memory<byte>> GetRoute(Memory<byte> firstRequestLine)
     {
@@ -41,10 +52,21 @@ internal sealed class Parser : IParser
         return new(firstRequestLine[(firstSpaceIndex+1)..lastSpaceIndex]); // space index + 1 to exclude whitespace and get first symbol of route
     }
 
+    /// <summary>
+    /// Extracts first request line represented in bytes
+    /// </summary>
+    /// <param name="request">Entire request needed to be parsed</param>
+    /// <returns>First request line represented in bytes</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private Memory<byte> GetFirstLine(Memory<byte> firstRequestLine) =>
-        firstRequestLine[..firstRequestLine.Span.IndexOf(RequestSymbolsAsBytes.CarriageReturnSymbol)];
+    private Memory<byte> GetFirstLine(Memory<byte> request) =>
+        request[..request.Span.IndexOf(RequestSymbolsAsBytes.CarriageReturnSymbol)];
 
+    /// <summary>
+    /// Extracts request method from entire request
+    /// </summary>
+    /// <param name="firstRequestLine">First line of the entire request</param>
+    /// <returns>The <see cref="Result{TResult}"/> wrapee with exception or method represented in bytes</returns>
+    /// <exception cref="ArgumentException">Returned if request does not contain method</exception>
     private Result<Memory<byte>> GetMethod(Memory<byte> firstRequestLine)
     {
         var spaceIndex = firstRequestLine.Span.IndexOf(RequestSymbolsAsBytes.Space);
@@ -54,52 +76,61 @@ internal sealed class Parser : IParser
         
         return new(firstRequestLine[..spaceIndex]);
     }
-
+    
+    /// <summary>
+    /// Splits the entire request to Headers and Body parts
+    /// </summary>
+    /// <param name="request">Entire request</param>
+    /// <returns>Tuple with slices of entire request parts. Body is null if request does not contain it</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private Result<(Memory<byte> Headers, Memory<byte>? Body)> SplitRequest(Memory<byte> request)
+    private (Memory<byte> Headers, Memory<byte>? Body) SplitRequest(Memory<byte> request)
     {
         var splitterIndex = request.Span.IndexOf(RequestSymbolsAsBytes.RequestSplitter);
 
         if (splitterIndex == -1)
-            return new((request, null));
+            return (request, null);
         
-        return new((request[..(splitterIndex + RequestSymbolsAsBytes.NewRequestLine.Length)], // NOTE: do not change, it is breaking change. Adding 1 new line symbol for proper header parsing 
-            request[(splitterIndex + RequestSymbolsAsBytes.RequestSplitter.Length)..]));
+        return (request[..(splitterIndex + RequestSymbolsAsBytes.NewRequestLine.Length)], // NOTE: do not change, it is breaking change. Adding 1 new line symbol for proper header parsing 
+            request[(splitterIndex + RequestSymbolsAsBytes.RequestSplitter.Length)..]);
     }
     
+    /// <summary>
+    /// Maps the entire request header section on specified headers.
+    /// </summary>
+    /// <param name="headers">The entire request header section</param>
+    /// <returns><see cref="Result{TResult}"/> wrapee with exception or headers dictionary.
+    /// The dictionaries key is header title without column</returns>
     [SkipLocalsInit]
     private Result<Dictionary<ReadOnlyMemory<byte>, ReadOnlyMemory<byte>>> MapHeaders(Memory<byte> headers)
     {
         var headersDictionary = new Dictionary<ReadOnlyMemory<byte>, ReadOnlyMemory<byte>>(8);
 
-        while (true)
+        while (headers.Length > 2)
         {
-            if (headers.Length < 2) break;
-
             var eol = headers.Span.IndexOf(RequestSymbolsAsBytes.NewLine);
-
 
             if (eol == -1)
             {
                 var colonIndex = headers.Span.IndexOf(RequestSymbolsAsBytes.Colon);
-             
-                if (colonIndex == -1) break;
-                headersDictionary.Add(headers[..colonIndex], headers[(colonIndex + 2)..]); // +2 to exclude colon and space,
+
+                if (colonIndex == -1) 
+                    return new(headersDictionary);
+                
+                headersDictionary.Add(headers[..colonIndex],headers[(colonIndex + 2)..]); // +2 to exclude colon and space,
+                
                 return new(headersDictionary);
             }
 
             var colon = headers.Span[..eol].IndexOf(RequestSymbolsAsBytes.Colon);
-            
+
             if (colon == -1)
                 return new(new FormatException("The headers had wrong format"));
-            
-            Debug.Assert(colon < eol);
 
             var key = headers[..colon];
-            var value = headers[(colon + 2)..(eol - 1)]; // +2 to exclude colon and space, -1 to exclude carriage return (\r) symbol
-            
-            headersDictionary[key] = value;
+            var value = headers[
+                (colon + 2)..(eol - 1)]; // +2 to exclude colon and space, -1 to exclude carriage return (\r) symbol
 
+            headersDictionary[key] = value;
             headers = headers[(eol + 1)..]; // +1 to exclude eol symbol
         }
 
--- src/LiteHttp.Server/Internal/Shared/Abstractions/Services/IParser.cs
+++ src/LiteHttp.Server/Internal/Shared/Abstractions/Services/IParser.cs
@@ -2,5 +2,10 @@
 
 public interface IParser
 {
+    /// <summary>
+    /// Parses the entire request bytes into <see cref="HttpContext"/> model.
+    /// </summary>
+    /// <param name="request">Entire request bytes.</param>
+    /// <returns></returns>
     Result<HttpContext> Parse(Memory<byte> request);
 }
