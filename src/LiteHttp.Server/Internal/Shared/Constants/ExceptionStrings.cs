7946244551d2f2ed5dfe6d05623da0b5b3a1a761
Archivelit <dalbaeb1488322666@gmail.com>
2025-11-28 12:29:30 +0100

    Add documentation to limits configuration

--- src/LiteHttp.Server/Internal/Shared/Constants/ExceptionStrings.cs
+++ src/LiteHttp.Server/Internal/Shared/Constants/ExceptionStrings.cs
@@ -3,6 +3,7 @@
 public static class ExceptionStrings
 {
     public const string NonNegativeNumberRequired = "Value must be greater than zero.";
-    public const string NullOrNonNegativeNumberRequired = "Value must be greater than zero or null.";
+    public const string NullOrPositiveNumberRequired = "Value must be greater than zero or null.";
     public const string PositiveTimeSpanRequired = "Value must be a positive TimeSpan.";
+    public const string NullZeroOrPositiveNumberRequired = "Value must be positive number or null.";
 }
--- src/LiteHttp.Server/Public/LimitsConfiguration.cs
+++ src/LiteHttp.Server/Public/LimitsConfiguration.cs
@@ -3,11 +3,33 @@
 
 namespace LiteHttp.Server;
 
-// TODO: Add documentation
-public class LimitsConfiguration
+/// <summary>
+/// <see cref="LimitsConfiguration"/> provides an api to configure limits of
+/// the <see cref="HttpServer"/> instance. 
+/// </summary>
+public sealed class LimitsConfiguration
 {
-    public LimitsConfiguration(Action<LimitsConfiguration> callback) => callback.Invoke(this);
+    /// <summary>
+    /// Configure limits using callback.
+    /// </summary>
+    /// <param name="callback">
+    /// A callback action that provides configuration.
+    /// </param>
+    public LimitsConfiguration(Action<LimitsConfiguration> callback) => 
+        callback?.Invoke(this);
 
+    /// <summary>
+    /// Gets or sets keep-alive timeout.
+    /// <para>
+    /// Defaults to 130 seconds.
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// Can be set to <see cref="Timeout.InfiniteTimeSpan"/>.
+    /// </remarks>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value less or equals <see cref="TimeSpan.Zero"/>.
+    /// </exception>
     public TimeSpan KeepAliveTimeout
     {
         get;
@@ -22,6 +44,18 @@ public class LimitsConfiguration
         }
     } = TimeSpan.FromSeconds(130);
 
+    /// <summary>
+    /// Gets or sets the request headers timeout.
+    /// <para>
+    /// Defaults to 30 seconds.
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// Can be set to <see cref="Timeout.InfiniteTimeSpan"/>.
+    /// </remarks>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is  less or equals <see cref="TimeSpan.Zero"/>.
+    /// </exception>
     public TimeSpan RequestHeadersTimeout
     {
         get;
@@ -36,78 +70,149 @@ public class LimitsConfiguration
         }
     } = TimeSpan.FromSeconds(30);
 
+    /// <summary>
+    /// Gets or sets the maximum of open concurrent connections.
+    /// When set to null, the limit is disabled.
+    /// <para>
+    /// Defaults to null (unlimited open connections).
+    /// </para>
+    /// </summary>
+    /// <param name="value">
+    /// Non-zero positive integer.
+    /// </param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is less or equals zero.
+    /// </exception>
     public long? MaxConcurrentConnections
     {
         get;
         set
         {
-            if (value is < 0)
-                throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NullOrNonNegativeNumberRequired);
+            if (value is <= 0)
+                throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NullOrPositiveNumberRequired);
 
             field = value;
         }
     }
-
+    
+    /// <summary>
+    /// Gets or sets the maximum number of open, upgraded connections.
+    /// When set to null, the number of upgraded connections is unlimited.
+    /// An upgraded connection is one that has been switched from HTTP to another protocol, such as WebSockets.
+    /// <para>
+    /// Defaults to null.
+    /// </para>
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is negative number.
+    /// </exception>
     public long? MaxConcurrentUpgradedConnections
     {
         get;
         set
         {
             if (value is < 0)
-                throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NullOrNonNegativeNumberRequired);
+                throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NullZeroOrPositiveNumberRequired);
 
             field = value;
         }
     }
 
+    /// <summary>
+    /// Gets or sets the maximum allowed count of headers in request.
+    /// <para>
+    /// Defaults to 100.
+    /// </para>
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is less or equals zero.
+    /// </exception> 
     public int MaxRequestHeaderCount
     {
         get;
         set
         {
-            if (value < 0)
+            if (value <= 0)
                 throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NonNegativeNumberRequired);
 
             field = value;
         }
     } = 100;
-
+    
+    /// <summary>
+    /// Gets or sets the maximum response buffer size.
+    /// If set to null, disables buffer limits.
+    /// <para>
+    /// Defaults to 64 kB (65,536 bytes).
+    /// </para>
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is less or equals zero.
+    /// </exception>
     public long? MaxResponseBufferSize
     {
         get;
         set
         {
-            if (value is < 0)
-                throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NullOrNonNegativeNumberRequired);
+            if (value is <= 0)
+                throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NullOrPositiveNumberRequired);
 
             field = value;
         }
     } = 64 * 1024;
-
+    
+    /// <summary>
+    /// Gets or sets the maximum size of request buffer.
+    /// If set to null, disables buffer limits.
+    /// <para>
+    /// Defaults to 1 MB (1,048,576 bytes).
+    /// </para>
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is less or equals zero.
+    /// </exception>
     public long? MaxRequestBufferSize
     {
         get;
         set
         {
-            if (value is < 0)
-                throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NullOrNonNegativeNumberRequired);
+            if (value is <= 0)
+                throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NullOrPositiveNumberRequired);
 
             field = value;
         }
     } = 1024 * 1024;
 
+    /// <summary>
+    /// Gets or sets maximum allowed size of request line.
+    /// <para>
+    /// Defaults to 8192 bytes (8 kB).
+    /// </para>
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is less or equals zero. 
+    /// </exception>
     public int MaxRequestLineSize
     {
         get;
         set
         {
-            if (value < 0)
+            if (value <= 0)
                 throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NonNegativeNumberRequired);
 
             field = value;
         }
     } = 8 * 1024;
 
+    /// <summary>
+    /// Gets or sets maximum allowed request body size.
+    /// <para>
+    /// Defaults to 30 000 000 bytes (approximately 28,6 MB).
+    /// </para>
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is negative number.
+    /// </exception>
     public long? MaxRequestBodySize
     {
         get;
@@ -118,14 +223,20 @@ public class LimitsConfiguration
 
             field = value;
         }
-    } = 30000000;
-
+    } = 30_000_000;
+    
+    /// <summary>
+    /// Gets or sets maximum allowed size for HTTP request headers.
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if value is less or equals zero 
+    /// </exception>
     public int MaxRequestHeadersTotalSize
     {
         get;
         set
         {
-            if (value < 0)
+            if (value <= 0)
                 throw new ArgumentOutOfRangeException(nameof(value), ExceptionStrings.NonNegativeNumberRequired);
 
             field = value;
