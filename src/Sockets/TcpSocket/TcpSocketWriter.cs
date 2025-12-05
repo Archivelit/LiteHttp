abed680bef4bb5256b9e44d13b1518f0a260d9e1
Archivelit <dalbaeb1488322666@gmail.com>
2025-12-05 12:21:41 +0100

    Add TcpSocketWriter documentation

--- src/Sockets/TcpSocket/TcpSocketWriter.cs
+++ src/Sockets/TcpSocket/TcpSocketWriter.cs
@@ -1,7 +1,16 @@
 ﻿namespace LiteHttp.Sockets.TcpSocket;
 
+/// <summary>
+/// This class is used for sending response data to a TCP client.
+/// </summary>
 internal class TcpSocketWriter
 {
+    /// <summary>
+    /// Asynchronously reads response data from the provided <see cref="Pipe"/> and sends it to the
+    /// specified TCP <see cref="Socket"/>.
+    /// </summary>
+    /// <param name="socket">A socket with connection where the response has to be sent.</param>
+    /// <param name="pipe">Pipe with response stored in it.</param>
     public async Task SendAsync(Socket socket, Pipe pipe)
     {
         while (true)
