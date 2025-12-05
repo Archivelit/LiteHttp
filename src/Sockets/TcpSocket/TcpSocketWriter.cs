f14a472f73f2ee5bfbc547243bda0e78401b7b47
Archivelit <dalbaeb1488322666@gmail.com>
2025-12-05 12:12:17 +0100

    Add initial version of TcpSocketWriter

---
+++ src/Sockets/TcpSocket/TcpSocketWriter.cs
@@ -0,0 +1,28 @@
+﻿namespace LiteHttp.Sockets.TcpSocket;
+
+internal class TcpSocketWriter
+{
+    public async Task SendAsync(Socket socket, Pipe pipe)
+    {
+        while (true)
+        {
+            var result = await pipe.Reader.ReadAsync();
+            if (result.IsCompleted)
+                break;
+            
+            try
+            {
+                foreach (var segment in result.Buffer)
+                    await socket.SendAsync(segment);
+            }
+            catch
+            {
+                break;
+            }
+
+            pipe.Reader.AdvanceTo(result.Buffer.Start, result.Buffer.End);
+        }
+
+        await pipe.Reader.CompleteAsync();
+    }
+}
