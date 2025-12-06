768b61cc3db22c73f08beaaa5c525bbc23647830
Adrian Z <sylzyg555@interia.pl>
2025-12-06 16:51:39 +0100

    added extra http status codes

--- src/LiteHttp.Server/Internal/Shared/Abstractions/Factories/IActionResultFactory.cs
+++ src/LiteHttp.Server/Internal/Shared/Abstractions/Factories/IActionResultFactory.cs
@@ -2,22 +2,75 @@
 
 public interface IActionResultFactory
 {
+    // 1xx Informational
+    public IActionResult Continue();
+    public IActionResult SwitchingProtocols();
+    public IActionResult Processing();
+    public IActionResult EarlyHints();
+
+    // 2xx Success
     public IActionResult Ok();
     public IActionResult Created();
     public IActionResult Accepted();
     public IActionResult NoContent();
+    public IActionResult ResetContent();
+    public IActionResult PartialContent();
+    public IActionResult MultiStatus();
+    public IActionResult AlreadyReported();
+    public IActionResult ImUsed();
+
+    // 3xx Redirection
     public IActionResult MultipleChoices();
+    public IActionResult MovedPermanently();
+    public IActionResult Found();
+    public IActionResult SeeOther();
     public IActionResult NotModified();
+    public IActionResult UseProxy();
+    public IActionResult SwitchProxy();
+    public IActionResult TemporaryRedirect();
+    public IActionResult PermanentRedirect();
+
+    // 4xx Client Errors
     public IActionResult BadRequest();
     public IActionResult Unauthorized();
+    public IActionResult PaymentRequired();
     public IActionResult Forbidden();
     public IActionResult NotFound();
     public IActionResult MethodNotAllowed();
+    public IActionResult NotAcceptable();
+    public IActionResult ProxyAuthenticationRequired();
     public IActionResult RequestTimeout();
     public IActionResult Conflict();
+    public IActionResult Gone();
+    public IActionResult LengthRequired();
+    public IActionResult PreconditionFailed();
     public IActionResult ContentTooLarge();
+    public IActionResult UriTooLong();
+    public IActionResult UnsupportedMediaType();
+    public IActionResult RangeNotSatisfiable();
+    public IActionResult ExpectationFailed();
+    public IActionResult ImATeapot();
+    public IActionResult MisdirectedRequest();
+    public IActionResult UnprocessableEntity();
+    public IActionResult Locked();
+    public IActionResult FailedDependency();
+    public IActionResult TooEarly();
+    public IActionResult UpgradeRequired();
+    public IActionResult PreconditionRequired();
     public IActionResult TooManyRequests();
+    public IActionResult RequestHeaderFieldsTooLarge();
+    public IActionResult UnavailableForLegalReasons();
+
+    // 5xx Server Errors
     public IActionResult InternalServerError();
     public IActionResult NotImplemented();
+    public IActionResult BadGateway();
     public IActionResult ServiceUnavailable();
+    public IActionResult GatewayTimeout();
+    public IActionResult HttpVersionNotSupported();
+    public IActionResult VariantAlsoNegotiates();
+    public IActionResult InsufficientStorage();
+    public IActionResult LoopDetected();
+    public IActionResult NotExtended();
+    public IActionResult NetworkAuthenticationRequired();
 }
--- tests/UnitTests/LiteHttp.RequestProcessors/ResponseBuilderTests.cs
+++ tests/UnitTests/LiteHttp.RequestProcessors/ResponseBuilderTests.cs
@@ -58,6 +58,30 @@ public class ResponseBuilderTests
         new List<object[]>
         {
             new object[]
+              {
+            new ActionResult(ResponseCode.Continue),
+            $"HTTP/1.1 100 Continue\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+            $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.SwitchingProtocols),
+                $"HTTP/1.1 101 Switching Protocols\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.Processing),
+                $"HTTP/1.1 102 Processing\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.EarlyHints),
+                $"HTTP/1.1 103 Early Hints\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
             {
                 new ActionResult(ResponseCode.Ok),
                 $"HTTP/1.1 200 OK\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
@@ -81,6 +105,24 @@ public class ResponseBuilderTests
                 $"HTTP/1.1 204 No Content\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
                 $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
             },
+             new object[]
+            {
+                new ActionResult(ResponseCode.MultiStatus),
+                $"HTTP/1.1 207 Multi Status\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.AlreadyReported),
+                $"HTTP/1.1 208 Already Reported\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.ImUsed),
+                $"HTTP/1.1 226 Im Used\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
             new object[]
             {
                 new ActionResult(ResponseCode.MultipleChoices),
@@ -89,8 +131,50 @@ public class ResponseBuilderTests
             },
             new object[]
             {
+                new ActionResult(ResponseCode.MovedPermanently),
+                $"HTTP/1.1 301 Moved Permanently\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.Found),
+                $"HTTP/1.1 302 Found\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.SeeOther),
+                $"HTTP/1.1 303 See Other\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
                 new ActionResult(ResponseCode.NotModified),
                 $"HTTP/1.1 304 Not Modified\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.UseProxy),
+                $"HTTP/1.1 305 Use Proxy\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.SwitchProxy),
+                $"HTTP/1.1 306 Switch Proxy\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.TemporaryRedirect),
+                $"HTTP/1.1 307 Temporary Redirect\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.PermanentRedirect),
+                $"HTTP/1.1 308 Permanent Redirect\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
                 $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
             },
             new object[]
@@ -103,6 +187,12 @@ public class ResponseBuilderTests
             {
                 new ActionResult(ResponseCode.Unauthorized),
                 $"HTTP/1.1 401 Unauthorized\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.PaymentRequired),
+                $"HTTP/1.1 402 Payment Required\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
                 $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
             },
             new object[]
@@ -125,6 +215,18 @@ public class ResponseBuilderTests
             },
             new object[]
             {
+                new ActionResult(ResponseCode.NotAcceptable),
+                $"HTTP/1.1 406 Not Acceptable\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.ProxyAuthenticationRequired),
+                $"HTTP/1.1 407 Proxy Authentication Required\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
                 new ActionResult(ResponseCode.RequestTimeout),
                 $"HTTP/1.1 408 Request Timeout\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
                 $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
@@ -135,6 +237,24 @@ public class ResponseBuilderTests
                 $"HTTP/1.1 409 Conflict\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
                 $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
             },
+             new object[]
+            {
+                new ActionResult(ResponseCode.Gone),
+                $"HTTP/1.1 410 Gone\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.LengthRequired),
+                $"HTTP/1.1 411 Length Required\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.PreconditionFailed),
+                $"HTTP/1.1 412 Precondition Failed\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
             new object[]
             {
                 new ActionResult(ResponseCode.ContentTooLarge),
@@ -143,8 +263,92 @@ public class ResponseBuilderTests
             },
             new object[]
             {
+                new ActionResult(ResponseCode.UriTooLong),
+                $"HTTP/1.1 414 URI Too Long\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.UnsupportedMediaType),
+                $"HTTP/1.1 415 Unsupported Media Type\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.RangeNotSatisfiable),
+                $"HTTP/1.1 416 Range Not Satisfiable\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.ExpectationFailed),
+                $"HTTP/1.1 417 Expectation Failed\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.ImATeapot),
+                $"HTTP/1.1 418 I'm a teapot\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.MisdirectedRequest),
+                $"HTTP/1.1 421 Misdirected Request\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.UnprocessableEntity),
+                $"HTTP/1.1 422 Unprocessable Entity\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.Locked),
+                $"HTTP/1.1 423 Locked\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.FailedDependency),
+                $"HTTP/1.1 424 Failed Dependency\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.TooEarly),
+                $"HTTP/1.1 425 Too Early\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.UpgradeRequired),
+                $"HTTP/1.1 426 Upgrade Required\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.PreconditionRequired),
+                $"HTTP/1.1 428 Precondition Required\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
                 new ActionResult(ResponseCode.TooManyRequests),
                 $"HTTP/1.1 429 Too Many Requests\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.RequestHeaderFieldsTooLarge),
+                $"HTTP/1.1 431 Request Header Fields Too Large\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.UnavailableForLegalReasons),
+                $"HTTP/1.1 451 Unavailable For Legal Reasons\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
                 $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
             },
             new object[]
@@ -161,8 +365,56 @@ public class ResponseBuilderTests
             },
             new object[]
             {
+                new ActionResult(ResponseCode.BadGateway),
+                $"HTTP/1.1 502 Bad Gateway\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
                 new ActionResult(ResponseCode.ServiceUnavailable),
                 $"HTTP/1.1 503 Service Unavailable\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.GatewayTimeout),
+                $"HTTP/1.1 504 Gateway Timeout\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.HttpVersionNotSupported),
+                $"HTTP/1.1 505 HTTP Version Not Supported\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.VariantAlsoNegotiates),
+                $"HTTP/1.1 506 Variant Also Negotiates\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.InsufficientStorage),
+                $"HTTP/1.1 507 Insufficient Storage\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.LoopDetected),
+                $"HTTP/1.1 508 Loop Detected\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.NotExtended),
+                $"HTTP/1.1 510 Not Extended\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
+                $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
+            },
+            new object[]
+            {
+                new ActionResult(ResponseCode.NetworkAuthenticationRequired),
+                $"HTTP/1.1 511 Network Authentication Required\r\nHost: {AddressConstants.IPV4_LOOPBACK.ToString()}" +
                 $":{AddressConstants.DEFAULT_SERVER_PORT}\r\n\r\n"
             }
         };
