e22884bdd88e1021a937d3ae805a24412018e372
Archivelit <dalbaeb1488322666@gmail.com>
2025-12-06 18:39:34 +0100

    Add #if DEBUG / #if RELEASE blocks to TcpSocketReader and TcpSocketWriter

--- src/Sockets/TcpSocket/TcpSocketReader.cs
+++ src/Sockets/TcpSocket/TcpSocketReader.cs
@@ -9,7 +9,8 @@ internal sealed class TcpSocketReader
     /// Minimal size of the buffer used when receiving request.
     /// </summary>
     private const int MinBufferSize = 1024;
-    
+
+#if RELEASE
     /// <summary>
     /// Asynchronously receives data from specified TCP <see cref="Socket"/> and writes
     /// them into the provided <see cref="Pipe"/>.
@@ -22,14 +23,14 @@ internal sealed class TcpSocketReader
         {
             try
             {
-                Memory<byte> buffer = pipe.Writer.GetMemory(MinBufferSize);
+                var buffer = pipe.Writer.GetMemory(MinBufferSize);
                 var bytesRead = await socket.ReceiveAsync(buffer);
 
                 if (bytesRead == 0)
                     break;
                 pipe.Writer.Advance(bytesRead);
             }
-            catch (Exception ex)
+            catch
             {
                 break;
             }
@@ -42,4 +43,40 @@ internal sealed class TcpSocketReader
 
         await pipe.Writer.CompleteAsync();
     }
+#endif
+
+#if DEBUG
+    /// <summary>
+    /// Asynchronously receives data from specified TCP <see cref="Socket"/> and writes
+    /// them into the provided <see cref="Pipe"/>.
+    /// </summary>
+    /// <param name="socket">The connected socket from which the request data is received.</param>
+    /// <param name="pipe">The pipe used to store received request data.</param>
+    public async Task ReceiveAsync(Socket socket, Pipe pipe)
+    {
+        while (true)
+        {
+            try
+            {
+                var buffer = pipe.Writer.GetMemory(MinBufferSize);
+                var bytesRead = await socket.ReceiveAsync(buffer);
+
+                if (bytesRead == 0)
+                    break;
+                pipe.Writer.Advance(bytesRead);
+            }
+            catch
+            {
+                break;
+            }
+            
+            var flushResult = await pipe.Writer.FlushAsync();
+
+            if (flushResult.IsCompleted)
+                break;
+        }
+
+        await pipe.Writer.CompleteAsync();
+    }
+#endif
 }
--- src/Sockets/TcpSocket/TcpSocketWriter.cs
+++ src/Sockets/TcpSocket/TcpSocketWriter.cs
@@ -5,6 +5,7 @@
 /// </summary>
 internal class TcpSocketWriter
 {
+#if RELEASE
     /// <summary>
     /// Asynchronously reads response data from the provided <see cref="Pipe"/> and sends it to the
     /// specified TCP <see cref="Socket"/>.
@@ -34,4 +35,37 @@ internal class TcpSocketWriter
 
         await pipe.Reader.CompleteAsync();
     }
+#endif
+
+#if DEBUG
+    /// <summary>
+    /// Asynchronously reads response data from the provided <see cref="Pipe"/> and sends it to the
+    /// specified TCP <see cref="Socket"/>.
+    /// </summary>
+    /// <param name="socket">A socket with connection where the response has to be sent.</param>
+    /// <param name="pipe">Pipe with response stored in it.</param>
+    public async Task SendAsync(Socket socket, Pipe pipe)
+    {
+        while (true)
+        {
+            var result = await pipe.Reader.ReadAsync();
+            if (result.IsCompleted)
+                break;
+
+            try
+            {
+                foreach (var segment in result.Buffer)
+                    await socket.SendAsync(segment);
+            }
+            catch
+            {
+                break;
+            }
+
+            pipe.Reader.AdvanceTo(result.Buffer.Start, result.Buffer.End);
+        }
+
+        await pipe.Reader.CompleteAsync();
+    }
+#endif
 }
