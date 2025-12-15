f75f7ac0bb9c5bea12d92f8f237834316850dcae
Archivelit <dalbaeb1488322666@gmail.com>
2025-12-15 13:57:49 +0100

    Add Parser tests

--- tests/UnitTests/LiteHttp.RequestProcessors/Pipeline/ParserTests.cs
+++ tests/UnitTests/LiteHttp.RequestProcessors/Pipeline/ParserTests.cs
@@ -30,10 +30,9 @@ public class ParserTests
         var result = await _parser.Parse(_requestPipe);
 
         // Assert
-
         if(!result.Success)
         {
-            TestContext.Current.TestOutputHelper.WriteLine(result.Error.Value.ErrorMessage);
+            _outputHelper.WriteLine(result.Error.Value.ErrorMessage);
             return;
         }
         WriteContextData(result.Value);
@@ -45,6 +44,146 @@ public class ParserTests
         actualHeaders.Should().BeEquivalentTo(expectedHeaders);
 
         result.Value.Body.Should().BeNull();
+    }
+    
+    [Fact]
+    public async ValueTask Parse_ValidRequest_WithoutBody_WithoutCRLFOnEnd_ShouldReturn_HttpContext()
+    {
+        // Arrange
+        var request = "GET / HTTP/1.1\r\nHost: test.com\r\n";
+
+        var expectedHeaders = new Dictionary<string, string>
+        {
+            { "Host", "test.com" }
+        };
+
+        var expectedRoute = Encoding.UTF8.GetBytes("/");
+        var expectedMethod = RequestMethodsAsBytes.Get;
+
+        await FillPipeWith(request);
+
+        // Act
+        var result = await _parser.Parse(_requestPipe);
+
+        // Assert
+        if(!result.Success)
+        {
+            _outputHelper.WriteLine(result.Error.Value.ErrorMessage);
+            return;
+        }
+        WriteContextData(result.Value);
+
+        result.Value.Method.Span.ToArray().Should().BeEquivalentTo(expectedMethod);
+        result.Value.Route.Span.ToArray().Should().BeEquivalentTo(expectedRoute);
+
+        ParseHeadersToStrings(result.Value, out var actualHeaders);
+        actualHeaders.Should().BeEquivalentTo(expectedHeaders);
+
+        result.Value.Body.Should().BeNull();
+    }
+
+    [Fact] public async ValueTask Parse_ValidRequest_WithSimpleBody_ShouldReturn_HttpContext()
+    {
+        // Arrange
+        var request = "PUT / HTTP/1.1\r\nHost: test.com\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, World!";
+
+        var expectedHeaders = new Dictionary<string, string>(3)
+        {
+            { "Host", "test.com" },
+            { "Content-Type", "text/plain" },
+            { "Content-Length", "13" }
+        };
+
+        var expectedRoute = Encoding.UTF8.GetBytes("/");
+        var expectedMethod = RequestMethodsAsBytes.Put;
+        var expectedBody = Encoding.UTF8.GetBytes("Hello, World!");
+
+        await FillPipeWith(request);
+        
+        // Act
+        var result = await _parser.Parse(_requestPipe);
+
+        // Assert
+        if(!result.Success)
+        {
+            _outputHelper.WriteLine(result.Error.Value.ErrorMessage);
+            return;
+        }
+        WriteContextData(result.Value);
+        
+        result.Value.Method.Span.ToArray().Should().BeEquivalentTo(expectedMethod);
+        result.Value.Route.Span.ToArray().Should().BeEquivalentTo(expectedRoute);
+
+        ParseHeadersToStrings(result.Value, out var actualHeaders);
+        actualHeaders.Should().BeEquivalentTo(expectedHeaders);
+
+        result.Value.Body.Should().NotBeNull();
+        result.Value.Body.Value.ToArray().Should().BeEquivalentTo(expectedBody);
+    }
+
+    [Fact]
+    public async ValueTask Parse_InvalidRequest_WithoutHttpVersion_ShouldReturn_ResultWithError_InvalidRequestSyntax()
+    {
+        // Arrange
+        var request = "GET /\r\nHost: test.com";
+    
+        await FillPipeWith(request);
+        
+        // Act
+        var result = await _parser.Parse(_requestPipe);
+
+        // Assert
+        result.Success.Should().BeFalse();
+        result.Error.Value.Should().NotBeNull().And.BeOfType<Error>();
+        result.Error.Value.ErrorCode.Should().Be(ParserErrors.InvalidRequestSyntax);
+    }
+
+    [Fact]
+    public async ValueTask Parse_InvalidRequest_WithoutMethod_ShouldReturn_ResultWithError_InvalidRequestSyntax()
+    {
+        // Arrange
+        var request = "/ HTTP1.0\r\nHost: test.com";
+
+        await FillPipeWith(request);
+        // Act
+        var result = await _parser.Parse(_requestPipe);
+
+        // Assert
+        result.Success.Should().BeFalse();
+        result.Error.Value.Should().NotBeNull().And.BeOfType<Error>();
+        result.Error.Value.ErrorCode.Should().Be(ParserErrors.InvalidRequestSyntax);
+    }
+
+    [Fact]
+    public async ValueTask Parse_InvalidRequest_WithoutRoute_ShouldReturn_ResultWithError_InvalidRequestSyntax()
+    {
+        // Arrange
+        var request = "GET HTTP1.0\r\nHost: test.com";
+
+        await FillPipeWith(request);
+        // Act
+        var result = await _parser.Parse(_requestPipe);
+
+        // Assert
+        result.Success.Should().BeFalse();
+        result.Error.Value.Should().NotBeNull().And.BeOfType<Error>();
+        result.Error.Value.ErrorCode.Should().Be(ParserErrors.InvalidRequestSyntax);
+    }
+
+    [Fact]
+    public async ValueTask Parse_InvalidRequest_WithoutSpaces_ShouldReturn_ResultWithError_InvalidRequestSyntax()
+    {
+        // Arrange
+        var request = "GET/HTTP1.0\r\nHost: test.com";
+
+        await FillPipeWith(request);
+        // Act 
+        var result = await _parser.Parse(_requestPipe);
+
+        // Assert
+        result.Success.Should().BeFalse();
+        result.Error.Should().BeOfType<Error>();
+        result.Error.Value.ErrorCode.Should().Be(ParserErrors.InvalidRequestSyntax);
     }
 
     private async ValueTask FillPipeWith(string request)
