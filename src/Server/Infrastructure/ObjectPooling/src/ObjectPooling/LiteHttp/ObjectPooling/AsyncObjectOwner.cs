7aa61e060145b75fcff8f3765b384dbe3fdac794
Archivelit <dalbaeb1488322666@gmail.com>
2025-12-24 18:05:17 +0100

    Fix AsyncObjectOwner api

--- src/Server/Infrastructure/ObjectPooling/src/ObjectPooling/LiteHttp/ObjectPooling/AsyncObjectOwner.cs
+++ src/Server/Infrastructure/ObjectPooling/src/ObjectPooling/LiteHttp/ObjectPooling/AsyncObjectOwner.cs
@@ -1,15 +1,42 @@
-﻿namespace LiteHttp.ObjectPooling;
+﻿using System.Diagnostics;
+
+namespace LiteHttp.ObjectPooling;
 
 public sealed class AsyncObjectOwner<Obj> : IAsyncDisposable where Obj : class
 {
     private readonly Func<Obj, ValueTask> _returnCallback;
-    public readonly Obj Object;
+    private bool _returned;
+
+    public Obj Object 
+    {
+        get 
+        {
+            Debug.Assert(!_returned, "Accessing returned object");
+            if (!_returned)
+            {
+                return field;
+            }
+            throw new InvalidOperationException("Cannot access returned object");
+        } 
+    }
 
     public AsyncObjectOwner(Obj @object, Func<Obj, ValueTask> returnCallback)
     {
         Object = @object;
         _returnCallback = returnCallback;
+        _returned = false;
     }
 
-    public ValueTask DisposeAsync() => _returnCallback(Object);
+    public ValueTask DisposeAsync()
+    {
+        Debug.Assert(!_returned, "Object returned second time");
+        
+        if (!_returned)
+        {
+            _returned = true;
+            return _returnCallback(Object);
+        }
+        
+        throw new InvalidOperationException("Object cannot be returned to pool second time");
+    }
 }
