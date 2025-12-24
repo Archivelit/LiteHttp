600f3f47d7a3fedf8a1718b0362707a981d4d1ab
Archivelit <dalbaeb1488322666@gmail.com>
2025-12-24 16:31:58 +0100

    Add initalization helper for ObjectPool

---
+++ src/Server/Infrastructure/ObjectPooling/src/ObjectPooling/LiteHttp/ObjectPooling/InitializationHelpers/ObjectPoolInitializationHelper.cs
@@ -0,0 +1,57 @@
+﻿namespace LiteHttp.ObjectPooling.InitializationHelpers;
+
+internal static class ObjectPoolInitializationHelper<TObject> where TObject : class
+{
+    public static void Initalize(int objectCount, ObjectPool<TObject> pool, Func<TObject> factory)
+    {
+        // Do not use Parallel.For here as not guaranteed to be thread safe
+        for (int i = 0; i < objectCount; i++)
+        {
+            var obj = CreateObject(factory);
+            pool._pool.Writer.TryWrite(obj);
+        }
+    }
+
+    public static async ValueTask InitalizeAsync(int objectCount, ObjectPool<TObject> pool, Func<ValueTask<TObject>> factory, CancellationToken cancellationToken = default)
+    {
+        // Do not use Parallel.For here as not guaranteed to be thread safe
+        for (int i = 0; i < objectCount; i++)
+        {
+            TObject obj;
+            try
+            {
+                obj = await factory().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                ExceptionDispatchInfo.Capture(ex).Throw();
+                throw; // Unreachable, but required to satisfy the compiler
+            }
+            await pool._pool.Writer.WriteAsync(obj, cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    public static async ValueTask InitalizeAsync(int objectCount, ObjectPool<TObject> pool, Func<TObject> factory, CancellationToken cancellationToken = default)
+    {
+        // Do not use Parallel.For here as not guaranteed to be thread safe
+        for (int i = 0; i < objectCount; i++)
+        {
+            await pool._pool.Writer.WriteAsync(CreateObject(factory), cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    private static TObject CreateObject(Func<TObject> factory)
+    {
+        TObject obj;
+        try
+        {
+            obj = factory();
+        }
+        catch
+        {
+            throw;
+        }
+
+        return obj;
+    }
+}
