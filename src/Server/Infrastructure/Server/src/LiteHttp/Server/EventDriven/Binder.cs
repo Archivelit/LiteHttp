627faa271a5f624bb730bdd1d1e64b6260161244
Archivelit <dalbaeb1488322666@gmail.com>
2026-01-06 12:24:43 +0100

    Add events for cases where error returned

--- src/Server/Infrastructure/Server/src/LiteHttp/Server/EventDriven/Binder.cs
+++ src/Server/Infrastructure/Server/src/LiteHttp/Server/EventDriven/Binder.cs
@@ -1,4 +1,6 @@
-﻿namespace LiteHttp.Server.EventDriven;
+﻿using LiteHttp.RequestProcessors;
+
+namespace LiteHttp.Server.EventDriven;
 
 internal static class Binder
 {
@@ -10,5 +12,9 @@ internal static class Binder
         server.RouterAdapter.SubscribeToCompleted(server.ExecutorAdapter.Handle);
         server.ExecutorAdapter.SubscribeToExecuted(server.ResponseBuilderAdapter.Handle);
         server.ResponseBuilderAdapter.SubscriveResponseBuilded(server.ConnectionManager.SendResponse);
+        
+        server.ParserAdapter.SubscribeToParsingError(server.ResponseBuilderAdapter.Handle);
+        
+        server.RouterAdapter.SubscribeToRequestNotFound(server.ResponseBuilderAdapter.Handle);
     }
 }
