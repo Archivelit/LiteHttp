bbb0e4908c10e6f2755a3dada33adbe093b52ea0
Archivelit <vladyslavshumilo@gmail.com>
2026-04-21 19:54:47 +0200

    Update connection manager behaviour if saea pool is empty

--- src/Server/Infrastructure/ConnectionManager/src/LiteHttp.ConnectionManager/LiteHttp/ConnectionManager/ConnectionManager.cs
+++ src/Server/Infrastructure/ConnectionManager/src/LiteHttp.ConnectionManager/LiteHttp/ConnectionManager/ConnectionManager.cs
@@ -18,38 +18,45 @@ public sealed class ConnectionManager : IHeartbeatHandler, IDisposable
 {
     private const int MinimalReceiveSpeed = 1024; // 1 KB/s 
     private static readonly TimeSpan Second = TimeSpan.FromSeconds(1);
-    
+
     private readonly DefaultObjectPool<SocketAsyncEventArgs> _saeaPool = new();
     private readonly ConcurrentDictionary<long, ConnectionContext> _connections;
     private readonly ConnectionContextFactory _connectionContextFactory = new();
-    
+
     public ConnectionManager()
     {
         const int initObjectsCount = 50000;
 
         _connections = new ConcurrentDictionary<long, ConnectionContext>(-1, initObjectsCount);
-        
+
         ObjectPoolInitializationHelper<SocketAsyncEventArgs>.Initialize(initObjectsCount, _saeaPool, () =>
         {
-            const int bufferSize = 4 * 1024; // 4 KB
-            var saea = new SocketAsyncEventArgs();
-
-            saea.Completed += IoCompleted;
-            saea.SetBuffer(new byte[bufferSize], 0, bufferSize);
+            var saea = InitSaea();
 
             return saea;
         });
     }
-    
+
+    private SocketAsyncEventArgs InitSaea()
+    {
+        const int bufferSize = 4 * 1024; // 4 KB
+
+        var saea = new SocketAsyncEventArgs();
+
+        saea.Completed += IoCompleted;
+        saea.SetBuffer(new byte[bufferSize], 0, bufferSize);
+        return saea;
+    }
+
     public void OnHeartbeat()
     {
         var now = DateTime.UtcNow;
-        
+
         foreach (var kvp in _connections)
         {
             var connection = kvp.Value;
-            
-            var lifetime =  now - connection.CreatedAtUtc;
+
+            var lifetime = now - connection.CreatedAtUtc;
             if (lifetime < Second) continue;
 
             var speed = connection.BytesReceived / lifetime.TotalSeconds;
@@ -57,13 +64,13 @@ public sealed class ConnectionManager : IHeartbeatHandler, IDisposable
                 CloseConnection(connection.SocketEventArgs);
         }
     }
-    
+
     public void HandleAccept(SocketAsyncEventArgs acceptEventArg)
     {
-        if (!_saeaPool.TryGet(out var saea)) return;
-        
+        if (!_saeaPool.TryGet(out var saea)) saea = InitSaea();
+
         saea.AcceptSocket = acceptEventArg.AcceptSocket;
-        
+
         ThreadPool.UnsafeQueueUserWorkItem(InitializeConnection, saea, false);
     }
 
@@ -72,11 +79,11 @@ public sealed class ConnectionManager : IHeartbeatHandler, IDisposable
         var connectionContext = _connectionContextFactory.Create(saea);
 
         saea.UserToken = connectionContext;
-        
+
         // REVIEW: not thread safe. Should be refactored to support multiple accept loops
         if (!_connections.TryAdd(connectionContext.Id, connectionContext))
             throw new InvalidOperationException($"Cannot add task {connectionContext.Id}");
-        
+
         Receive(saea);
     }
 
@@ -93,14 +100,14 @@ public sealed class ConnectionManager : IHeartbeatHandler, IDisposable
     {
         var socket = connectionContext.SocketEventArgs.AcceptSocket;
         bool willRaiseEvent = socket.SendAsync(connectionContext.SocketEventArgs);
-        
+
         if (!willRaiseEvent)
             ProcessSend(connectionContext.SocketEventArgs);
     }
-    
+
     private void IoCompleted(object? sender, SocketAsyncEventArgs saea)
     {
-        switch(saea.LastOperation)
+        switch (saea.LastOperation)
         {
             case SocketAsyncOperation.Receive:
                 ProcessReceive(saea);
@@ -117,17 +124,17 @@ public sealed class ConnectionManager : IHeartbeatHandler, IDisposable
         saea.AcceptSocket = null;
         saea.UserToken = null;
         saea.SetBuffer(0, saea.Buffer.Length);
-        
+
         _saeaPool.TryReturn(saea);
     }
 
     private void CloseConnection(SocketAsyncEventArgs saea)
     {
         var connectionContext = (ConnectionContext)saea.UserToken;
-        
+
         if (!_connections.TryRemove(connectionContext.Id, out _))
             throw new InvalidOperationException($"Cannot remove connection {connectionContext.Id}");
-        
+
         saea.AcceptSocket.Shutdown(SocketShutdown.Both);
         saea.AcceptSocket.Close();
     }
@@ -135,9 +142,9 @@ public sealed class ConnectionManager : IHeartbeatHandler, IDisposable
     private void ProcessReceive(SocketAsyncEventArgs saea)
     {
         var connectionContext = (ConnectionContext)saea.UserToken;
-    
+
         connectionContext.IncrementBytesReceived(saea.BytesTransferred);
-        
+
         saea.SetBuffer(saea.Offset, saea.Offset + saea.BytesTransferred);
         OnDataReceived(connectionContext);
     }
